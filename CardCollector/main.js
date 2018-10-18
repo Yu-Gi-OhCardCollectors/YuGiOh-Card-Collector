a0684f504e0daf69ba3712ef0b40b61e7fdde024
Austin Cain <austincain22@gmail.com>
2018-10-18 14:34:50 -0700

    HUGE changes
    -removes different pages for different decks by using javascript.
    -Added decks.js to handle this.
    -changes whole button to be a link instead of just the word for tabs.
    -background color to lightpink :)

--- CardCollector/main.js
+++ CardCollector/main.js
@@ -8,30 +8,3 @@ function placeImages(){
 	}
 }
 placeImages();
-
-//Deck 1
-function one(){
-
-}
-
-//Deck 2
-function two(){
-
-}
-
-//Deck 3
-function three(){
-
-}
-
-// Add active class to the current button (highlight it)
-//still needs work but it's supposed to cycle between what deck is chosen to be viewed
-var header = document.getElementById("btns");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = header.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
