8044eb0f02a862af096520f3ea4cbbc0f3e9f805
Miklo-Delgado <miklo_delgado@mail.fresnostate.edu>
2018-11-08 16:47:23 -0800

    Code for the Firebase login/signup
    This is the code for firebase. You will have to use visual studio code. Also will have to have firbase setup

---
+++ CardCollector/app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoader", event => {
+    const app = firebase.app();
+    console.log(app)
+});
+
+
+
+function googleLogin(){
+    alert("hello world");
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider) //this is what actually signs people in
+
+            .then(result => {                               //this displays hello to the user who just signed in
+                const user = result.user;
+                document.write(`Hello ${user.displayName}`);
+                console.log(user)
+            } )
+            .catch(console.log)
+}
+
