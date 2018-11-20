d91d7a6130e15220ef482c318749c88308c9dc40
Austin Cain <austincain22@gmail.com>
2018-11-20 13:03:16 -0800

    Updates how images are displayed
    removes button being wrapped around the image. instead it just puts the onclick function as an attribute of the img tag.

--- public/main.js
+++ public/main.js
@@ -4,13 +4,11 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);	  //parses json file and stores it in myObj
         for(var i = 0; i < 7986; ++i){
-			var cardBtn = document.createElement('BUTTON');
-			cardBtn.setAttribute('onclick', 'cardDecide("'+myObj.cards[i].imageUrl+'",'+i+')');
-				var cardImage = document.createElement('IMG');			//creates <img> tag in html
-				cardImage.setAttribute('src', myObj.cards[i].imageUrl);  //image path
-				cardImage.setAttribute('width', '150');
-				cardBtn.appendChild(cardImage);
-				document.getElementById('cardImages').appendChild(cardBtn); //puts <img with path> into the ID with cardImages
+			var cardImage = document.createElement('IMG');			//creates <img> tag in html
+			cardImage.setAttribute('src', myObj.cards[i].imageUrl);  //image path
+			cardImage.setAttribute('width', '150');
+			cardImage.setAttribute('onclick', 'cardDecide("'+myObj.cards[i].imageUrl+'",'+i+')');	//onclick function on the image
+			document.getElementById('cardImages').appendChild(cardImage); //puts <img with path> into the ID with cardImages
 		}
     }
 };
