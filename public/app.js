3f6e0e8724b494e815d7c5519f434e6bc40abb72
KlyeXinog <42728371+KlyeXinog@users.noreply.github.com>
2018-11-20 19:57:09 -0800

    Login stays consistent throughout tabs
    Authentication stays consistent throughout the tabs.
    
    Changed code for the login button
    Added code for the logout button
    
    In app.js:
      Added script for firebase hosting. (Change to your firebase hosting when testing)
    
     added firebase.auth().onAuthStateChanged(function(user)
    
     to check the state of authentiation.
    
    Problems:
       Authentication works but the web pages load all the buttons first then removes buttons based on the authentication state.

--- public/app.js
+++ public/app.js
@@ -1,22 +1,56 @@
+var config = {
+    apiKey: "AIzaSyB3oUvB1QikK3UWahKGjoSmKjEhC4mDvGQ",
+    authDomain: "yugiohcards-7dd7d.firebaseapp.com",
+    databaseURL: "https://yugiohcards-7dd7d.firebaseio.com",
+    projectId: "yugiohcards-7dd7d",
+    storageBucket: "yugiohcards-7dd7d.appspot.com",
+    messagingSenderId: "314192197528"
+};
+
+  firebase.initializeApp(config);
+
 document.addEventListener("DOMContentLoader", event => {
     const app = firebase.app();
     console.log(app)
 });
 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      document.getElementById('welcome').innerHTML = `Welcome ${user.displayName}`;
+      document.getElementById("logout").style.display = "block";
+      document.getElementById("login").style.display = "none";
+      //document.getElementById("user_div").style.display = "block";
+      //document.getElementById("login_div").style.display = "none";
+  
+      var user = firebase.auth().currentUser;
+  
+      if(user != null){
+  
+        var email_id = user.email;
+        //document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+  
+      }
+  
+    } else {
+      // No user is signed in.
+      document.getElementById('welcome').innerHTML = "Welcome to YuGiOh Card Collector";
+      document.getElementById("login").style.display = "block";
+      document.getElementById("logout").style.display = "none";
+  
+    }
+  });
 
 
 function googleLogin(){
-    alert("hello world");
+    alert("Logging In"); //an alert to let us know the function was called
     const provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(provider) //this is what actually signs people in 
             
             .then(result => {                               //this displays hello to the user who just signed in
                 const user = result.user;
-                document.getElementById('welcome').innerHTML = `Welcome ${user.displayName}`;
-                document.getElementById('loginBut').innerHTML = "Log out";  //This will replace the login button with a log out one
-                document.getElementById('loginBut').setAttribute('onclick', 'googleLogout()');  //On a click of the log out button it will call
-                                                                                                //the googleLogout function
+                
                 console.log(user)
             } )
             .catch(console.log)
@@ -27,11 +61,7 @@ function googleLogout(){
     alert("Sign out");  //an alert to let us know the function was called
 
     firebase.auth().signOut().then(function() { //signs user out
-        // Sign-out successful.
-
-        document.getElementById('welcome').innerHTML = `Welcome to YuGiOh Card Collector`;  //After log out it will display OG greeting
-        document.getElementById('loginBut').innerHTML = "Log in with google";               //Changes the button back to user login
-        document.getElementById('loginBut').setAttribute('onclick', 'googleLogin()');       //On a click of the button calls googleLogin function
+    
         
       }).catch(function(error) {
         // An error happened.
